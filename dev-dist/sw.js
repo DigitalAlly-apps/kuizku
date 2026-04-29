5aaeaa5ad5f68a21bd656a9a4dc5713ea658a470
DigitalAlly <digitalally.apps@gmail.com>
2026-04-29 10:07:10 +0700

    fix UI layout hpp

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ca201e7p69g"
+    "revision": "0.jh6c9fl03v4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
